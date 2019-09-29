30f02be83c5305953869694fbabefc60c92d3f34
amitozs999 <ekas0001@gmail.com>
2019-09-29 18:38:49 +0530

    commit

---
+++ app/src/main/java/com/weather/forecastmvvm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.weather.forecastmvvm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.weather.forecastmvvm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
